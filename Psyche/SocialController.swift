5017f8494aa50ba236824166f1f00f38c6f3ae85
NA <NA@janells-imac.local>
2018-01-07 16:54:39 -0700

    Added Psyche Timeline

--- Psyche/SocialController.swift
+++ Psyche/SocialController.swift
@@ -9,24 +9,16 @@
 import UIKit
 import TwitterKit
 
-class SocialController: UIViewController{
+class SocialController: TWTRTimelineViewController{
     
     let client = TWTRAPIClient()
     
-    @IBOutlet var Tweet: TWTRTweetView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Twitter.sharedInstance().start(withConsumerKey: "tzMhH05lzEgNcAn4EB48CPnFA", consumerSecret: "6GYaAVzcGCM3f3OabouqFrM2H0SBFt7hcQzO60HxGV6mIbtomJ")
-        client.loadTweet(withID: "949406863413555200") { (tweet, error) in
-            if let t = tweet {
-                self.Tweet.configure(with: t)
-            } else {
-                print("Failed to load Tweet: \(error!.localizedDescription)")
-            }
-        }    }
+        self.dataSource = TWTRUserTimelineDataSource(screenName: "NASAPsyche", apiClient: client)
+    }
+ 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()// Dispose of any resources that can be recreated.
-        
     }
 }
