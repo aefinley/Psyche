25ad8acf8d868c26af6e277595e1b5493a91857a
Timothy Cook <Chase@timothys-mbp.mobile.asu.edu>
2018-01-22 12:15:52 -0700

    added media gallery view controller, working on image views

---
+++ Psyche/views/imageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  imageCollectionViewCell.swift
+//  Psyche
+//
+//  Created by Timothy Cook on 1/22/18.
+//  Copyright © 2018 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class imageCollectionViewCell: UICollectionViewCell {
+    
+}
