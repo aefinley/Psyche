38fed72b4f70ee90c0b54e224c80d3981f387086
Jennifer Krause <jkrause7@asu.edu>
2018-01-28 15:11:23 -0700

    Adding timeline mvc

---
+++ Psyche/TimelineViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TimelineViewController.swift
+//  Psyche
+//
+//  Created by Allison Finley on 1/18/18.
+//  Copyright © 2018 Apple Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TimelineViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        // set background to purple
+        super.viewDidLoad()
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.1843, green: 0.1255, blue: 0.2745, alpha: 1.0)
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        
+        // memory stuff
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func menuPress(_ sender: Any) {
+         self.navigationController?.setNavigationBarHidden(false, animated: true)
+        // todo
+    }
+    
+    @IBAction func firstTimeline(_ sender: Any) {
+        
+    }
+    
+    
+    
+}
