7d9741d0108a18a502eb2ba91ba917a8a4d59dae
Allison Finley <aefinley@asu.edu>
2018-02-11 20:03:26 -0700

    added table view template for mission categories and added detailed view for mission facts

--- Psyche/MissionFactsViewController.swift
+++ Psyche/MissionFactsViewController.swift
@@ -2,7 +2,7 @@
 //  MissionFactsViewController.swift
 //  Psyche
 //
-//  Created by Dustin Hart on 1/18/18.
+//  Created by Allison Finley on 1/18/18.
 //  Copyright © 2018 Apple Inc. All rights reserved.
 //
 
