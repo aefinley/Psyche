d5cbc153315411e5ae935738b7b8ff31e66285d5
NA <NA@janells-imac.local>
2018-03-11 20:52:42 -0700

    added the beginnings of the media gallery (images only)

--- Psyche/MediaGallery.swift
+++ Psyche/MediaGallery.swift
@@ -11,41 +11,56 @@ import UIKit
 
 class MediaGallery: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    @IBOutlet weak var collectionView: UICollectionView!
-    
-    var imageArray: [UIImage] = [ UIImage(named: "1")!, UIImage(named: "2")!, UIImage(named: "3")!, UIImage(named: "4")!, UIImage(named: "5")!, UIImage(named: "6")!, UIImage(named: "7")!, UIImage(named: "8")!, UIImage(named: "9")! ]
+    var imageArray: [UIImage] = [UIImage]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+        getImages()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    override func didReceiveMemoryWarning() { super.didReceiveMemoryWarning() }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imageArray.count
-    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int { return imageArray.count }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        
-        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath) as! imageCollectionViewCell
-        
-       // let image = UIImage(named: "myImageName")
-        let object = imageArray[indexPath.item]
-        //cell.imageView.image = image
-
-        
-        
-        //cell.imageView.image = image
-        
+        cell.imageview.image = imageArray[indexPath.row]
         return cell
     }
     
-    
+    func getImages(){
+        var pagedata:String = ""
+        var i = 1, added = 16
+        while added == 16 {
+            added=0
+            let url: URL = URL(string: "https://psyche.asu.edu/galleries/images/page/" + String(i))!
+            do{
+                try pagedata = String(contentsOf: url)
+            } catch _ {}
+            let arr = pagedata.split(separator: "<")
+            var findend=false
+            finder: for str in arr {
+                if findend{
+                    let stri = str.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
+                    if stri == "/section>" {
+                        break finder
+                    }
+                    if stri.hasPrefix("img") {
+                        let looker = stri.split(separator: " ")[3]
+                        //var end = looker.suffix(5)
+                        //looker = looker.prefix(upTo: looker.index(looker.endIndex, offsetBy: -13)) + end
+                        //print(looker)
+                        do{
+                            try imageArray.append(UIImage(data: Data(contentsOf: URL(string: String(looker.suffix(from: looker.index(looker.startIndex, offsetBy: 5))).trimmingCharacters(in: CharacterSet(charactersIn: "\"")))!))!)
+                        } catch _ {}
+                        added+=1
+                    }
+                }
+                if str.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines) == "section class=\"gallery-grid container\">" {
+                    findend=true
+                }
+            }
+            i+=1
+        }    }
 }
